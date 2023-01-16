a6460d359a4396382c26104e135f6c6fa83d3870
RodrigoD324 <rodrigo.silvadeoliveira324@gmail.com>
2023-01-16 17:53:38 -0300

    Criação do JSP 'formAlteraEmpresa.jsp' e da classe 'MostraEmpresaServlet.java'

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -44,4 +44,13 @@ public class Banco {
 		}
 	}
 
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+
 }
